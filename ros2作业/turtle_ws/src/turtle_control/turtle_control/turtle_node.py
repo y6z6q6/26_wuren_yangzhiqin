0131ac37dd29a6f51f1054e7bb2d5a7eed96cf63
y6z6q6 <2076366968@qq.com>
2026-06-07 02:43:46 +0800

    合并远程代码 + 新增CNN作业

--- ros2作业/turtle_ws/src/turtle_control/turtle_control/turtle_node.py
+++ ros2作业/turtle_ws/src/turtle_control/turtle_control/turtle_node.py
@@ -6,7 +6,8 @@ import math
 class Turtle(Node):
     #初始化
     def __init__(self):
-        super().__init__("turtle_control_node")
+        super().__init__("turtle_control_node")#创建节点名字为turtle_control_node
+        #初始化节点参数数值
         self.declare_parameters(
             namespace="",
             parameters=[
@@ -24,17 +25,20 @@ class Turtle(Node):
         #发出速度话题
         self.publisher = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
         timer_period = 1.0 / self.pinlv
-        #设定更新频率
+        #设定更新频率，每timer_period秒，就进入timer_callback函数一次
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.start_time = self.get_clock().now().nanoseconds / 1e9
     #回调函数
     def timer_callback(self):
+    	#计算现在时间
         now = self.get_clock().now().nanoseconds / 1e9
         t = now - self.start_time
         omega = self.jiaosudu * math.sin(2 * math.pi * t / self.zhouqi)
+        #初始化twist为Twist数据类型
         twist = Twist()
         twist.linear.x = self.xiansudu
         twist.angular.z = omega
+        #发布速度话题
         self.publisher.publish(twist)
 
 def main(args=None):
